6a68dcf4c99168794760a9369f9fef01edb0ac01
Yura Sabat <yrasabat@gmail.com>
2022-08-03 13:26:15 +0300

    start_routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Link, Route, Routes} from "react-router-dom";
+import {Home} from "./poges/Home";
+import {About} from "./poges/About";
+import {Blog} from "./poges/Blog";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className={'mainDiv'}>
+            <header>
+                <Link className={'link'} to={'/'}>
+                    <div className={'linkDiv'}>
+                        Home
+                    </div>
+                </Link>
+                <Link className={'link'} to={'/about'}>
+                    <div className={'linkDiv'}>
+                        About
+                    </div>
+                </Link>
+                <Link className={'link'} to={'/blog'}>
+                    <div className={'linkDiv'}>
+                        Blog
+                    </div>
+                </Link>
+            </header>
+            <main>
+                <Routes>
+                    <Route path={'/'} element={<Home/>}/>
+                    <Route path={'/about'} element={<About/>}/>
+                    <Route path={'/blog'} element={<Blog/>}/>
+                    <Route path={'*'} element={<Home/>}/>
+                </Routes>
+            </main>
+            <footer>
+                Footer
+            </footer>
+        </div>
+    );
 }
 
 export default App;
