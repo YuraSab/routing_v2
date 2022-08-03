bb3cfd3588384b251f0ab88d89a93aeb3cef0378
Yura Sabat <yrasabat@gmail.com>
2022-08-03 17:48:33 +0300

    Search

---
+++ src/components/BlogFilter/BlogFilter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BlogFilter = () => {
+ 
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export { BlogFilter }
--- src/poges/Blog/Blog.js
+++ src/poges/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Link} from "react-router-dom";
+import {Link, useSearchParams} from "react-router-dom";
 import styles from "./Blog.module.css";
 
 const Blog = () => {
@@ -9,13 +9,18 @@ const Blog = () => {
     const [postsOnPage, setPostsOnPage] = useState([]);
     const [page, setPage] = useState(1);
 
+    const [searchParams, setSearchParams] = useSearchParams();
+    const postQuery = searchParams.get('post') || '';
+
+    const latest = searchParams.has('latest');
+    const startsFrom = latest ? 80 : 1;
 
 
-    const fetchData = async() => {
+    const fetchData = async () => {
         const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
         const json = await response.json();
         setPosts(json);
-        const onPage = await json.filter(el => el.id <= (page*20) && el.id > (page === 1 ? 0 : page*20-20));
+        const onPage = await json.filter(el => el.id <= (page * 20) && el.id > (page === 1 ? 0 : page * 20 - 20));
         // .then(value => setPostsOnPage(value))
         setPostsOnPage(onPage);
     }
@@ -26,30 +31,50 @@ const Blog = () => {
     }, [page])
 
 
-
-
-
     const nextPage = () => {
-        setPage(prevState => prevState+1)
+        setPage(prevState => prevState + 1)
     }
     const prevPage = () => {
-        setPage(prevState => prevState-1)
+        setPage(prevState => prevState - 1)
+    }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const query = form.search.value;
+        // setSearchParams({post: query});
+        const isLatest = form.latest.checked;
+        const params = {};
+
+        if(query.length) params.post = query;
+        if(isLatest) params.latest = true;
+
+        setSearchParams(params)
     }
 
 
     // const pages = Math.max(posts.length/20);
     // const pages = Math.round(posts.length/20);
-    const pages = Math.floor(posts.length/20);
-    // console.log(pages);
+    const pages = Math.floor(posts.length / 20);
 
     return (
-
-
         <div style={{minHeight: 599}}>
             <h1>Blogs:</h1>
+
+            <form onSubmit={handleSubmit} autoComplete={'off'}>
+                <input type={'search'} name={'search'}/>
+                <label style={{padding: '0 1rem'}}> New only
+                    <input type={'checkbox'} name={'latest'}/>
+                </label>
+                <input type={'submit'} value={'Search'}/>
+            </form>
+
             <Link to={'/blog/new'}>Add new post</Link>
             {
-                postsOnPage.map(post => (
+                postsOnPage.filter(
+                    post => post.title.includes(postQuery) && post.id >= startsFrom
+                ).map(post => (
                     <Link
                         to={`/blog/${post.id}`}
                         key={post.id}
